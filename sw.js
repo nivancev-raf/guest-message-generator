de7571a6f9d3b553421375d878f324132855411f
Nikola Ivancev <115183102+nivancev-raf@users.noreply.github.com>
2025-08-31 14:36:10 +0200

    init commit

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'guest-message-generator-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Install event
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
